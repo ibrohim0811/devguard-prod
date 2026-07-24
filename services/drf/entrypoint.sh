1aa9fc18df54f5766469ccd799525de08403d4aa
ibrohim <dangercoder046@gmail.com>
2026-07-24 20:07:27 +0500

    entrpoint.sh fixed for celery worker

--- services/drf/entrypoint.sh
+++ services/drf/entrypoint.sh
@@ -1,11 +1,19 @@
 #!/bin/sh
 
-# Exception yuzaga kelsa skriptni to'xtatish
+# Xatolik bo'lsa skriptni to'xtatish
 set -e
 
 # manage.py joylashgan papkaga o'tamiz
 cd /app/services/drf
 
+# Agar biror alohida buyruq berilgan bo'lsa (masalan, docker-compose dagi `command: celery...`)
+# o'sha buyruqni ishga tushiramiz:
+if [ $# -gt 0 ]; then
+    echo "⚡ Berilgan buyruq bajarilmoqda: $@"
+    exec "$@"
+fi
+
+# Agar hech qanday buyruq berilmagan bo'lsa (default holatda Django server uchun):
 echo "🗄️ Migratsiyalar bajarilmoqda..."
 python manage.py migrate --noinput
 
