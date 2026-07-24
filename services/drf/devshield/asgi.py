7f1d6c6b5886353f5a24c73c5f9877e335c27c63
ibrohim <dangercoder046@gmail.com>
2026-07-24 21:41:40 +0500

    asgi changed

--- services/drf/devshield/asgi.py
+++ services/drf/devshield/asgi.py
@@ -1,13 +1,19 @@
 import os
 from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
-import users.routing  # routing faylingiz yo'li
 
+# 1. Django settings moduli o'rnatiladi
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'devshield.settings')
 
+# 2. Django ilovasi initialize qilinadi (IMPORTLARDAN OLDIN!)
+django_asgi_app = get_asgi_application()
+
+# 3. Muhit tayyor bo'lgach, Channels va routing import qilinadi
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+import users.routing
+
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
+    "http": django_asgi_app,
     "websocket": AuthMiddlewareStack(
         URLRouter(
             users.routing.websocket_urlpatterns
