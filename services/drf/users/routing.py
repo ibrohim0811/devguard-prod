9eaa4bfa9d33be33484b41d2255a9c45e9443632
ibrohim <dangercoder046@gmail.com>
2026-07-24 23:30:17 +0500

    routing py changed

--- services/drf/users/routing.py
+++ services/drf/users/routing.py
@@ -2,5 +2,5 @@ from django.urls import re_path
 from users import consumers  # 👈 relative (.) o'rniga direct import qilib ko'ring
 
 websocket_urlpatterns = [
-    re_path(r'^/ws/scan/(?P<task_id>[\w-]+)/$', consumers.ScanProgressConsumer.as_asgi()),
+    re_path(r'ws/scan/(?P<task_id>[\w-]+)/$', consumers.ScanProgressConsumer.as_asgi()),
 ]
