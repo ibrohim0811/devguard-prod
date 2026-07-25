0aa8148187eab1c59b6609ff61f226492f3f073c
ibrohim <dangercoder046@gmail.com>
2026-07-25 11:54:12 +0500

    asgi application changed

--- services/drf/devshield/asgi.py
+++ services/drf/devshield/asgi.py
@@ -1,22 +1,21 @@
 import os
 from django.core.asgi import get_asgi_application
 
-# 1. Django sozlamalarini ko'rsatamiz
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'devshield.settings')
 
-# 2. Django ASGI ilovasini yuklaymiz (BOSHQA IMPORTLARDAN OLDIN!)
+# 1. Django HTTP application
 django_asgi_app = get_asgi_application()
 
-# 3. Importlarni get_asgi_application() dan KEYIN qilamiz
+# 2. Routing va Channels importlari
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-import users.routing  # users app ichidagi routing.py
+import users.routing  # 👈 users.routing import qilinganiga e'tibor bering
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
     "websocket": AuthMiddlewareStack(
         URLRouter(
-            users.routing.websocket_urlpatterns
+            users.routing.websocket_urlpatterns  # 👈 Mana shu joyda ulangan bo'lishi shart
         )
     ),
 })
--- services/drf/users/routing.py
+++ services/drf/users/routing.py
@@ -1,6 +1,7 @@
 from django.urls import re_path
-from users import consumers  # 👈 relative (.) o'rniga direct import qilib ko'ring
+from users import consumers  # yoki o'zingizning consumer joylashgan faylingiz
 
 websocket_urlpatterns = [
-    re_path(r'ws/scan/(?P<task_id>[\w-]+)/$', consumers.ScanProgressConsumer.as_asgi()),
+    # [\w-]+ orqali harflar, raqamlar va '-' (UUID) belgilari ham qamrab olinadi:
+    re_path(r'^ws/scan/(?P<task_id>[\w-]+)/?$', consumers.ScanProgressConsumer.as_asgi()),
 ]
