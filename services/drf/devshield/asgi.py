7f867c46c18929f3d04cf2d384b7c51490732242
ibrohim <dangercoder046@gmail.com>
2026-07-25 16:34:40 +0500

    asgi application changed

--- services/drf/devshield/asgi.py
+++ services/drf/devshield/asgi.py
@@ -9,13 +9,16 @@ django_asgi_app = get_asgi_application()
 # 2. Routing va Channels importlari
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
+from channels.security.websocket import AllowedHostsOriginValidator
 import users.routing  # 👈 users.routing import qilinganiga e'tibor bering
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
-    "websocket": AuthMiddlewareStack(
-        URLRouter(
-            users.routing.websocket_urlpatterns  # 👈 Mana shu joyda ulangan bo'lishi shart
+    "websocket": AllowedHostsOriginValidator(
+        AuthMiddlewareStack(
+            URLRouter(
+                users.routing.websocket_urlpatterns  # 👈 Mana shu joyda ulangan bo'lishi shart
+            )
         )
     ),
 })
