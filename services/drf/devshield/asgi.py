db864b162ced4b3bd45a7d89573cce2304646d8b
ibrohim <dangercoder046@gmail.com>
2026-07-25 23:50:39 +0500

    authmiddlware stack added on asgi application

--- services/drf/devshield/asgi.py
+++ services/drf/devshield/asgi.py
@@ -3,16 +3,15 @@ from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'devshield.settings')
 
-# 1. Django HTTP app initialize
 django_asgi_app = get_asgi_application()
 
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-import users.routing  # routing.py faylingiz
+import users.routing
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
-    "websocket": AuthMiddlewareStack(
+    "websocket": AuthMiddlewareStack( # 👈 AllowedHostsOriginValidator siz sinab ko'ring
         URLRouter(
             users.routing.websocket_urlpatterns
         )
