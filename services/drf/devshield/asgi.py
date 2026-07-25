f7a05ef4bc7745471c387e8fb3db019e18bd17a3
ibrohim <dangercoder046@gmail.com>
2026-07-25 23:11:29 +0500

    nginx conf and asgi application updated and fixed

--- services/drf/devshield/asgi.py
+++ services/drf/devshield/asgi.py
@@ -3,22 +3,18 @@ from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'devshield.settings')
 
-# 1. Django HTTP application
+# 1. Django HTTP app initialize
 django_asgi_app = get_asgi_application()
 
-# 2. Routing va Channels importlari
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-from channels.security.websocket import AllowedHostsOriginValidator
-import users.routing  # 👈 users.routing import qilinganiga e'tibor bering
+import users.routing  # routing.py faylingiz
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
-    "websocket": AllowedHostsOriginValidator(
-        AuthMiddlewareStack(
-            URLRouter(
-                users.routing.websocket_urlpatterns  # 👈 Mana shu joyda ulangan bo'lishi shart
-            )
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            users.routing.websocket_urlpatterns
         )
     ),
 })
--- services/drf/users/routing.py
+++ services/drf/users/routing.py
@@ -1,8 +1,6 @@
-from django.urls import path, re_path
+from django.urls import re_path
 from users import consumers
 
 websocket_urlpatterns = [
-    path('ws/scan/<str:task_id>/', consumers.ScanProgressConsumer.as_asgi()),
-    path('ws/scan/<str:task_id>', consumers.ScanProgressConsumer.as_asgi()),
-    re_path(r'^/?ws/scan/(?P<task_id>[a-zA-Z0-9-]+)/?$', consumers.ScanProgressConsumer.as_asgi()),
+    re_path(r'^ws/scan/(?P<task_id>[\w-]+)/?$', consumers.ScanProgressConsumer.as_asgi()),
 ]
